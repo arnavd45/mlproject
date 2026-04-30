576fe1aec88d5506c2dc7677361e7755d7449de4
Arnav Deshpande <arnavdeshpande0311@gmail.com>
2026-05-01 02:22:53 +0530

    Hyperparameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -19,6 +19,7 @@ from src.exception import CustomException
 from src.logger import logging
 from src.utils import save_object,evaluate_models
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 @dataclass
@@ -38,19 +39,63 @@ class ModelTrainer:
 
             X_test = test_array[:, :-1]
             y_test = test_array[:, -1]
-            models={
-                "Random Forest":RandomForestRegressor(),
-                "Adaboost Regressor":AdaBoostRegressor(),
-                "Linear Regression":LinearRegression(),
-                "Gradient boost":GradientBoostingRegressor(),
-                "KNN":KNeighborsRegressor(),
-                "Catboost Regressor":CatBoostRegressor(verbose=0),
-                "Xgboost":XGBRegressor(verbosity=0),
-                "SVM Regressor":SVR(),
-                "Decision Tree":DecisionTreeRegressor()
-            }
+            models = {
+                 "Random Forest": RandomForestRegressor(),
+                 "Adaboost Regressor": AdaBoostRegressor(),
+                 "Linear Regression": LinearRegression(),
+                 "Gradient Boosting": GradientBoostingRegressor(),
+                 "KNN": KNeighborsRegressor(),
+                 "Catboost Regressor": CatBoostRegressor(verbose=0),
+                 "Xgboost": XGBRegressor(verbosity=0),
+                 "SVM Regressor": SVR(),
+                 "Decision Tree": DecisionTreeRegressor()
+                }
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            params={
+                "Decision Tree":{
+                    "criterion":['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    "splitter":['best', 'random'],
+                    "max_features":['sqrt', 'log2']
+                },
+                "Random Forest":{
+                    "criterion":['squared_error', 'absolute_error', 'friedman_mse', 'poisson'],
+                    "max_features":['sqrt', 'log2'],
+                    "n_estimators":[100,200,300,400]
+                },
+                "Adaboost Regressor":{
+                    "n_estimators":[100,200,300,400],
+                    "learning_rate":[1,0.1,0.01,0.001],
+                    "loss":['linear', 'square', 'exponential']
+                },
+                "Gradient Boosting":{
+                    "loss":['squared_error', 'absolute_error', 'huber', 'quantile'],
+                    "n_estimators":[100,200,300,400],
+                    "subsample": [0.6, 0.7, 0.8, 0.9]
+                },
+                "Catboost Regressor":{
+                    "depth":[6,8,10],
+                    "learning_rate":[.1,.01,.05,.001],
+                    "n_estimators":[100,200,300,400]
+
+                },
+                "Linear Regression":{},
+                "KNN":{
+                    "n_neighbors":[5,7,9,11],
+                    "weights":['uniform', 'distance'],
+                    "algorithm":['auto', 'ball_tree', 'kd_tree', 'brute']
+                },
+                "SVM Regressor":{
+                    "gamma": ['scale', 'auto']
+                },
+                
+                "Xgboost": {
+                "n_estimators": [100, 200, 300],
+                "learning_rate": [0.01, 0.1, 0.2],
+                "max_depth": [3, 5, 7]
+                }
+
+            }
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
 
 
             best_model_score=max(model_report.values())
